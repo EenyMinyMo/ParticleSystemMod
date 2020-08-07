52f9bfccbde3c2c07ac899ca0b24347d0a53c7c3
SomberLaptop <SomberSoul43@gmail.com>
2020-08-07 05:19:22 +0500

    В тестовом режиме написаны шейдеры для отрисовки частиц через них.

--- src/main/resources/assets/somber_particle_system/shader/simple_shader_particle/particle_frag.glsl
+++ src/main/resources/assets/somber_particle_system/shader/simple_shader_particle/particle_frag.glsl
@@ -1,5 +1,15 @@
 #version 330
 
-void main() {
+uniform sampler2D particleTexture;
 
+in vec2 texCoord;
+in float alpha;
+in float light;
+
+out vec4 fragColor;
+
+void main() {
+    vec4 texel = vec4(texture(particleTexture, texCoord).rgb * light, alpha);
+
+    fragColor = texel;
 }
--- src/main/resources/assets/somber_particle_system/shader/simple_shader_particle/particle_vert.glsl
+++ src/main/resources/assets/somber_particle_system/shader/simple_shader_particle/particle_vert.glsl
@@ -1,5 +1,55 @@
 #version 330
 
-void main() {
+layout (location = 0) in vec3 particlePosition;
+layout (location = 1) in vec3 particleLocalAngles;
+layout (location = 2) in vec2 particleTextureCoord;
+layout (location = 3) in vec4 particleAttribute;
+layout (location = 4) in vec3 particleCenterPosition;
 
+//uniform mat4 projectionMatrix;
+//uniform mat4 cameraMatrix;
+//uniform mat4 modelMatrix;
+uniform mat4 projectionAndCameraMatrix;
+uniform vec3 cameraPosition;
+
+out vec2 texCoord;
+out float alpha;
+out float light;
+
+mat4 modelTranformMat;
+
+/*
+Вычисляет матрицу трансформации модели.
+Матрица трансформации модели - по сути совмещенная матрица
+для поворота за вектором lookAtParticle (вектор направления частицы)
+и переносом в позицию частицы.
+*/
+void computeModelTranformMat() {
+    vec3 lookAtParticle = cameraPosition - particleCenterPosition;
+
+    vec3 forward = normalize(lookAtParticle);
+    vec3 up = vec3(0, 1, 0);
+    vec3 left = cross(up, forward);
+    up = cross(forward, left);
+
+    //Устанавливаем новый базис для матрицы трансформации.
+    //строка здесь - столбец для матрицы на самом деле.
+    modelTranformMat = mat4(
+    left.x,              left.y,             left.z,            0,
+    up.x,                up.y,               up.z,              0,
+    forward.x,           forward.y,          forward.z,         0,
+    -lookAtParticle.x,  -lookAtParticle.y,  -lookAtParticle.z,  1
+    );
+}
+
+void main() {
+    texCoord = particleTextureCoord;
+    alpha = particleAttribute.x;
+    light = particleAttribute.y;
+
+    vec2 halfSizes = particleAttribute.zw;
+    particlePosition.x *= halfSizes.x;
+    particlePosition.y *= halfSizes.y;
+
+    gl_Position = projectionAndCameraMatrix * modelTranformMat * vec4(particlePosition, 1);
 }
